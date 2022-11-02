446f5e4f6ab73f380bdcfe3300b713739f3e02b6
Sri Vidya Nalluri <111886796+vidya976@users.noreply.github.com>
2022-11-02 18:53:25 -0500

    added

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/zara', zaraRouter);
-app.use('/gridbuild', gridbuildRouter);
+app.use('/gridbuild', gridbulidRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
